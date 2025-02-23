f0a229f6d4b62ec70e7e98ac923df4e782039d6d
Myrmex <dfusi@hotmail.com>
2025-02-23 20:00:46 +0100

    Infrastructure

--- Cadmus.Export.ML/AppLinearTextTreeRenderer.cs
+++ Cadmus.Export.ML/AppLinearTextTreeRenderer.cs
@@ -1,7 +1,13 @@
-﻿using Cadmus.Export.Filters;
+﻿using Cadmus.Core;
+using Cadmus.Export.Filters;
+using Cadmus.General.Parts;
+using Cadmus.Philology.Parts;
+using Fusi.Tools.Configuration;
 using Fusi.Tools.Data;
+using Proteus.Text.Xml;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace Cadmus.Export.ML;
 
@@ -9,31 +15,109 @@ namespace Cadmus.Export.ML;
 /// Linear text tree with single apparatus layer renderer.
 /// </summary>
 /// <seealso cref="ITextTreeRenderer" />
-public sealed class AppLinearTextTreeRenderer : ITextTreeRenderer
+public sealed class AppLinearTextTreeRenderer : TextTreeRenderer, ITextTreeRenderer,
+    IConfigurable<AppLinearTextTreeRendererOptions>
 {
     /// <summary>
-    /// Gets the optional filters to apply after the renderer completes.
+    /// The context block type data key to retrieve it from the context data.
     /// </summary>
-    public IList<IRendererFilter> Filters { get; } = [];
+    public const string CONTEXT_BLOCK_TYPE = "block-type";
+
+    private AppLinearTextTreeRendererOptions _options = new();
 
     /// <summary>
-    /// Renders the specified tree.
+    /// Configures this renderer with the specified options.
     /// </summary>
-    /// <param name="tree">The tree.</param>
-    /// <param name="context">The rendering context.</param>
-    /// <returns>Rendition.</returns>
-    /// <exception cref="ArgumentNullException">tree or context</exception>
-    public string Render(TreeNode<TextSpanPayload> tree, IRendererContext context)
+    /// <param name="options">The options.</param>
+    public void Configure(AppLinearTextTreeRendererOptions options)
+    {
+        _options = options ?? new AppLinearTextTreeRendererOptions();
+    }
+
+    /// <summary>
+    /// Renders the specified JSON code.
+    /// </summary>
+    /// <param name="tree">The root node of the text tree.</param>
+    /// <param name="context">The optional renderer context.</param>
+    /// <returns>Rendered output.</returns>
+    /// <exception cref="ArgumentNullException">tree</exception>
+    protected override string DoRender(TreeNode<TextSpanPayload> tree,
+        IRendererContext? context = null)
     {
         ArgumentNullException.ThrowIfNull(tree);
-        ArgumentNullException.ThrowIfNull(context);
 
+        // get the root element name
+        XName rootName = _options.ResolvePrefixedName(_options.RootElement);
+
+        // get the block name
+        string blockType = "default";
+        if (context != null &&
+            context.Data.TryGetValue(CONTEXT_BLOCK_TYPE, out object? value))
+        {
+            blockType = value as string ?? "default";
+        }
+
+        XName blockName = _options.ResolvePrefixedName(
+            _options.DefaultNsPrefix + ":" + _options.BlockElements[blockType]);
+
+        // calculate fragment ID prefix
+        string prefix = GetFragmentPrefixFor(
+            new TokenTextLayerPart<ApparatusLayerFragment>(),
+            new ApparatusLayerFragment());
+
+        // create root element
+        XElement root = new(rootName);
+
+        // traverse nodes and build the XML
         tree.Traverse(node =>
         {
-
+            // TODO
             return true;
         });
 
-        throw new NotImplementedException();
+        return root.ToString(_options.Indented
+            ? SaveOptions.OmitDuplicateNamespaces
+            : SaveOptions.OmitDuplicateNamespaces | SaveOptions.DisableFormatting);
+    }
+}
+
+/// <summary>
+/// Options for <see cref="AppLinearTextTreeRenderer"/>.
+/// </summary>
+/// <seealso cref="XmlTextFilterOptions" />
+public class AppLinearTextTreeRendererOptions : XmlTextFilterOptions
+{
+    /// <summary>
+    /// Gets or sets the name of the root element. The default is <c>div</c>.
+    /// </summary>
+    public string RootElement { get; set; }
+
+    /// <summary>
+    /// Gets or sets the block element name(s). The default name is "p" under
+    /// a <c>default</c> key, other names can be specified for conditional
+    /// element names (e.g. when dealing with poetry rather than prose).
+    /// If you need to specify a namespaced name, use the format "prefix:name"
+    /// and define the prefix in the <see cref="XmlTextFilterOptions.Namespaces"/>
+    /// property.
+    /// </summary>
+    public IDictionary<string, string> BlockElements { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the XML output should be
+    /// indented. This can be useful for diagnostic purposes.
+    /// </summary>
+    public bool Indented { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="AppLinearTextTreeRendererOptions"/> class.
+    /// </summary>
+    public AppLinearTextTreeRendererOptions()
+    {
+        RootElement = "div";
+        BlockElements = new Dictionary<string, string>
+        {
+            ["default"] = "p"
+        };
     }
 }
--- Cadmus.Export/ITextTreeRenderer.cs
+++ Cadmus.Export/ITextTreeRenderer.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Cadmus.Export.Filters;
+﻿using Cadmus.Export.Filters;
 using Fusi.Tools.Data;
 
 namespace Cadmus.Export;
