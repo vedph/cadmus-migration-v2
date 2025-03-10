0d1608183cc632b6ff765d611efbcfdcfef4fed7
Myrmex <dfusi@hotmail.com>
2025-03-10 12:14:36 +0100

    Update AppParallelTextTreeFilterTest.cs

--- Cadmus.Export.Test/Filters/AppParallelTextTreeFilterTest.cs
+++ Cadmus.Export.Test/Filters/AppParallelTextTreeFilterTest.cs
@@ -5,6 +5,7 @@ using Cadmus.Philology.Parts;
 using Fusi.Tools.Data;
 using System;
 using System.Collections.Generic;
+using Xunit;
 
 namespace Cadmus.Export.Test.Filters;
 
@@ -58,11 +59,119 @@ public sealed class AppParallelTextTreeFilterTest
         });
 
         // ludere O1GR: luderem O | loedere Trappers-Lomax, 2007 69
-        // TODO
+        part.Fragments.Add(new ApparatusLayerFragment()
+        {
+            Location = "1.2",
+            Entries =
+            [
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Note,
+                    IsAccepted = true,
+                    Witnesses =
+                    [
+                        new AnnotatedValue { Value = "O1" },
+                        new AnnotatedValue { Value = "G" },
+                        new AnnotatedValue { Value = "R" }
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "luderem",
+                    Witnesses =
+                    [
+                        new AnnotatedValue { Value = "O" },
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "loedere",
+                    Authors =
+                    [
+                        new LocAnnotatedValue
+                        {
+                            Value = "Trappers-Lomax",
+                            Location= "2007 69"
+                        },
+                    ]
+                }
+            ]
+        });
 
         // possem OGR: possum MS48 | possim Turnebus, 1573 26 |
         // posse Vossius, 1684 | posset Heinsius, dub. 1646-81
-        // TODO
+        part.Fragments.Add(new ApparatusLayerFragment()
+        {
+            Location = "1.5",
+            Entries =
+            [
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Note,
+                    IsAccepted = true,
+                    Witnesses =
+                    [
+                        new AnnotatedValue { Value = "O" },
+                        new AnnotatedValue { Value = "G" },
+                        new AnnotatedValue { Value = "R" }
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "possum",
+                    Witnesses =
+                    [
+                        new LocAnnotatedValue
+                        {
+                            Value = "MS48",
+                        }
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "possim",
+                    Authors =
+                    [
+                        new LocAnnotatedValue
+                        {
+                            Value = "Turnebus",
+                            Location = "1573 26"
+                        }
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "posse",
+                    Authors =
+                    [
+                        new LocAnnotatedValue
+                        {
+                            Value = "Vossius",
+                            Location = "1684"
+                        }
+                    ]
+                },
+                new ApparatusEntry
+                {
+                    Type = ApparatusEntryType.Replacement,
+                    Value = "posset",
+                    Authors =
+                    [
+                        new LocAnnotatedValue
+                        {
+                            Value = "Heinsius",
+                            Note = "dub.",
+                            Location = "1646-81"
+                        }
+                    ]
+                }
+            ]
+        });
 
         return part;
     }
@@ -91,9 +200,19 @@ public sealed class AppParallelTextTreeFilterTest
         // build a linear tree from ranges
         TreeNode<TextSpan> tree = ItemComposer.BuildTreeFromRanges(
             mergedRanges, tr.Item1);
+
         // apply block filter
         return (new BlockLinearTextTreeFilter().Apply(tree, item), item);
     }
 
-    // TODO
+    [Fact]
+    public void Apply_Ok()
+    {
+        (TreeNode<TextSpan> tree, IItem item) = GetTreeAndItem();
+        AppParallelTextTreeFilter filter = new();
+
+        TreeNode<TextSpan> result = filter.Apply(tree, item);
+
+        Assert.NotNull(result);
+    }
 }
